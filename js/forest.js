a2e721a009a0d470a46d105d6849e050c1bbf1a1
Jack Mandelkorn <jacman444@gmail.com>
2018-03-18 13:52:15 -0700

    1.1.1

--- js/forest.js
+++ js/forest.js
@@ -1,4 +1,5 @@
 var globalSize = 1000;
+var defaultWidth = 150;
 var globalResult;
 
 var w = new Worker('js/worker.js');
@@ -6,7 +7,9 @@ w.onmessage = function(e) {
   var result = JSON.parse(JSON.parse(e.data));
   globalResult = result;
   for (var i = 0; i < result.items.length; i++) {
-    new Panel(result.items[i].image.width,result.items[i].image.height,(Math.round(Math.random() * globalSize) - (globalSize / 2)),0,(Math.round(Math.random() * globalSize) - (globalSize / 2)),("background-image:url(" + result.items[i].link + ")"));
+    var width = defaultWidth + Math.round(Math.random() * 10);
+    var height = Math.round((width / result.items[i].image.width) * result.items[i].image.height);
+    new Panel(width,height,(Math.round(Math.random() * globalSize) - (globalSize / 2)),0,(Math.round(Math.random() * globalSize) - (globalSize / 2)),("background-image:url(" + result.items[i].link + ")"));
   }
 }
 
